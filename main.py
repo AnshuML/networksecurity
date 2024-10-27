cb72fcdab699512f94d458d0dcf12824deef94ae
ANSHU <129033345+AnshuML@users.noreply.github.com>
2024-10-27 18:09:45 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from network_security.components.data_ingestion import DataIngestion
-#from network_security.components.data_validation import DataValidation
+from network_security.components.data_validation import DataValidation
 #from network_security.components.data_transformation import DataTransformation
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
@@ -21,12 +21,12 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
-        #data_validation_config=DataValidationConfig(trainingpipelineconfig)
-        #data_validation=DataValidation(dataingestionartifact,data_validation_config)
-        #logging.info("Initiate the data Validation")
-        #data_validation_artifact=data_validation.initiate_data_validation()
-        #logging.info("data Validation Completed")
-        #print(data_validation_artifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
         #data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
         #logging.info("data Transformation started")
         #data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
